97965e5127c2d9c048cc93aa902afab99381d192
Dhanraj Fasle <116111006+dhanrajfasle@users.noreply.github.com>
2024-07-01 11:03:10 -0700

    Update bundle.js

--- Netfix clone/bundle.js
+++ Netfix clone/bundle.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
-
+app.use(cors());
 
 const api = "api_key=995cdf2a72cb31d821a28f61ca3546ff";
 const base_url = "https://api.themoviedb.org/3";
